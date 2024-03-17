d45375c703a890051bf1b7f8dec9562210ab82a7
nicholasanayaputra <146567253+nicholasanayaputra@users.noreply.github.com>
2024-03-17 23:17:02 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar">
+        <h2 class="logo">trxvl.</h2>
+        <div class="nav-list">
+            <a href="#">Home</a>
+            <a href="/dashboard/tugaspwl/tour.php">Tours</a>
+            <a href="#">Contact</a>
+            <div class="profile">
+                <img src="./images/profile.jpg" alt="profile" class="profile-img">
+            </div>
+        </div>
+    </nav>
+
+    <!-- Hero -->
+    <div class="hero">
+        <div class="hero-wrapper">
+            <img src="https://images.unsplash.com/photo-1529519801205-697b55d1194e?q=80&w=1455&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <div class="hero-text">
+                <p class="title">All Inclusive</p>
+                <p class="subtitle">Enjoy Hiking & Getaways</p>
+                <p class="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas molestiae repellat maiores optio aspernatur maxime, molestias omnis. Rerum, id sint.</p>
+                <button class="btn">Reserve Now</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Offers -->
+    <div class="offers">
+        <p>Booking Incentive</p>
+        <p>Book With Flexibelity</p>
+        <p>Trip Insurance</p>
+        <p>Compotitive Rates</p>
+    </div>
+
+    <!-- Plans -->
+    <div class="plans">
+
+        <!-- Left Side -->
+        <div class="left-plans">
+            <img src="https://images.unsplash.com/photo-1575573685828-7c1e20f05124?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="mountain">
+            <img src="https://images.unsplash.com/photo-1671884424342-60245cd6a57b?q=80&w=1633&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="mountain">
+            <img src="https://images.unsplash.com/photo-1621001481154-a52cbb91fcc2?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="mountain">
+            <img src="https://images.unsplash.com/photo-1526494631344-8c6fa6462b17?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="mountain">
+            <img src="https://images.unsplash.com/photo-1705682534655-c39daa35ce2e?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="mountain">
+        </div>
+
+        <!-- Right Side -->
+        <div class="right-plans">
+            <h3>Plan Your Trip In Indonesia</h3>
+            <p class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, unde.</p>
+            <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt eaque exercitationem perspiciatis maiores, facilis aperiam eveniet quasi sequi! Alias veritatis eos, dolorum suscipit consequatur doloribus quidem in corporis labore fugit!</p>
+            <div class="buttons-plans">
+                <button>Learn More</button>
+                <button>Book Your Trip</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Camp -->
+    <div class="camp">
+        <div class="camp-text">
+            <h3>Fine Interior Camps</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus accusamus quae at quidem explicabo nemo.</p>
+        </div>
+
+        <div class="camp-images">
+            <img src="https://images.unsplash.com/photo-1565627797561-755e46e89270?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/">
+            <img src="https://images.unsplash.com/photo-1682688759157-57988e10ffa8?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/">
+            <img src="https://images.unsplash.com/photo-1690269841828-426c38556e99?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/">
+        </div>
+    </div>
+
+    <!-- Share -->
+    <div class="share">
+        <div class="share-text">
+            <h3>Take these tools to get to the top of the mountain</h3>
+            <h5>#Rinjani 3726 Mdpl</h5>
+        </div>
+        <div class="share-images">
+            <div class="left-share">
+                <img src="https://images.unsplash.com/photo-1588668214407-6ea9a6d8c272?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+            <div>
+                <img src="https://images.unsplash.com/photo-1586685256769-4e869a64f1eb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+            <div>
+                <img src="https://images.unsplash.com/photo-1535576434247-e0f50b766399?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+            <div>
+                <img src="https://images.unsplash.com/photo-1679958908017-9544c3a90f50?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+            <div>
+                <img src="https://images.unsplash.com/photo-1485809052957-5113b0ff51af?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ tour.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="tour.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="tour">
+        <h1>Tours</h1>
+        <div class="tour-card">
+            <div>
+                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+            <div>
+                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+            <div>
+                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+            <div>
+                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+            <div>
+                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+            <div>
+                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
