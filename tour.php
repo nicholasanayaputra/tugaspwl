c9d2a1a963d77ff1db509d0a86dc51da28e8cd00
nicholasanayaputra <146567253+nicholasanayaputra@users.noreply.github.com>
2024-03-18 19:08:31 +0700

    change tours

--- tour.php
+++ tour.php
@@ -4,32 +4,90 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="tour.css">
+    <link rel="stylesheet" href="tours.css">
+
     <title>Document</title>
 </head>
 
 <body>
+    <nav class="navbar">
+        <h2 class="logo">trxvl.</h2>
+        <div class="nav-list">
+            <a href="/dashboard/tugaspwl/home.php">Home</a>
+            <a href="/dashboard/tugaspwl/tour.php">Tours</a>
+            <a href="#">Contact</a>
+            <div class="profile">
+                <img src="./images/profile.jpg" alt="profile" class="profile-img">
+            </div>
+        </div>
+    </nav>
     <div class="tour">
         <h1>Tours</h1>
         <div class="tour-card">
-            <div>
-                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <div class="card">
+                <img src="https://images.unsplash.com/photo-1519681393784-d120267933ba?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                <div>
+                    <h4>Mountain Rinjani</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
             </div>
-            <div>
-                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <div class="card">
+                <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?q=80&w=1517&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                <div>
+                    <h4>Mountain Salak</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
             </div>
-            <div>
-                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <div class="card">
+                <img src="https://images.unsplash.com/photo-1486728297118-82a07bc48a28?q=80&w=1529&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D    " alt="">
+                <div>
+                    <h4>Mountain Pangrango</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
             </div>
-            <div>
-                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <div class="card">
+                <img src="https://images.unsplash.com/photo-1469521669194-babb45599def?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                <div>
+                    <h4>Mountain Ciremai</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
             </div>
-            <div>
-                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <div class="card">
+                <img src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                <div>
+                    <h4>Mountain Sumbing</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
             </div>
-            <div>
-                <img src="https://plus.unsplash.com/premium_photo-1709311394823-8b0856db0bcc?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <div class="card">
+                <img src="https://images.unsplash.com/photo-1530947443747-bcaaacd048d0?q=80&w=1529&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                <div>
+                    <h4>Mountain Sindoro</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
             </div>
+            <div class="card">
+                <img src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                <div>
+                    <h4>Mountain Gede</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://images.unsplash.com/photo-1494253188410-ff0cdea5499e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                <div>
+                    <h4>Mountain Lawu</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://images.unsplash.com/photo-1498696815880-6fd23346fe56?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                <div>
+                    <h4>Mountain Bromo</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+            </div>
+
         </div>
     </div>
 </body>
